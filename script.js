0373c5cb74c21067fa897976050925c2b888a487
adrianrdguez <pollor67@gmail.com>
2020-05-26 18:29:04 +0100

    Casi trail
    Co-authored-by: David  <dav.ollebac@gmail.com>

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var ball
 var canvasWidth = document.body.clientWidth
+var positionsMax = 50;
+var positions = [];
+var xPos = 400;
+var yPos = 250
 
 function startGame() {
- ball = new component("white", 0, 0, 6);
+  ball = new component("white", xPos, yPos, 6);
   myGameArea.start();
 }
 
@@ -11,16 +15,15 @@ var myGameArea = {
   start: function () {
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.canvas.style.cursor = "crosshair"; //hide the original cursor
-    this.interval = setInterval(updateGameArea, 20);
-    window.addEventListener('mousemove', function (e) {
+    this.canvas.style.cursor = "crosshair";
+    this.interval = setInterval(updateGameArea, 15);
+    window.addEventListener('mousemove', function (e) { // object follows the mouse
       var rect = this.canvas.getBoundingClientRect();
       myGameArea.left = rect.left;
       myGameArea.top = rect.top
       myGameArea.x = e.pageX - myGameArea.left;
       myGameArea.y = e.pageY - myGameArea.top;
-      console.log(myGameArea.left)
-    })
+    });
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -40,7 +43,29 @@ function component(color, x, y, radius) {
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fillStyle = this.color;
-    ctx.fill()
+    ctx.fill();
+//
+let history = positions, prevPot, nextPot;
+for (let i = 0; i < history.length - 1; i++) {
+  ctx.beginPath();
+  ctx.arc(positions[i].x, positions[i].y, 1, 0, 2 * Math.PI, true);
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fill();
+  prevPot = history[i];
+  nextPot = history[i + 1];
+  if (i === 0) {
+    ctx.moveTo(prevPot.x, prevPot.y);
+  } else {
+    // ctx.quadraticCurveTo(prevPot.x, prevPot.y, prevPot.x + (nextPot.x - prevPot.x) / 2, prevPot.y + (nextPot.y - prevPot.y) / 2);
+    ctx.lineTo(nextPot.x, nextPot.y);
+  }
+
+  prevPot.x -= 2;
+  prevPot.y += 2;
+}
+
+storeLastPosition(this.x, this.y);
+
   }
 }
 
@@ -52,3 +77,14 @@ function updateGameArea() {
   }
   ball.update();
 }
+
+
+function storeLastPosition(xPos, yPos) {
+  positions.push({
+    x: xPos,
+    y: yPos
+  })
+  if (positions.length > positionsMax) {
+    positions.splice(0, positions.length > positionsMax);
+  }
+};
