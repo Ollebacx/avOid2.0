acd6d680bf775e17be75c8b60cd81542276576d8
Ollebacx <dav.ollebac@gmail.com>
2020-05-28 10:06:14 +0100

    Refactor in components and Map Canvas working

---
+++ game.js
@@ -0,0 +1,8 @@
+
+function Game() {
+  this.map = new Map()
+  map.create()
+}
+
+// canvas.width = window.innerWidth > 1000 ? window.innerWidth - 100 : window.innerWidth
+// canvas.height = window.innerHeight > 400 ? window.innerHeight - 20 : window.innerHeight
---
+++ js/map.js
@@ -0,0 +1,24 @@
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
+
+function Map() {
+  this.create = function () {
+    // this.canvas = config.canvas;
+    this.canvas = document.getElementById('canvas');
+    this.context = this.canvas.getContext('2d')
+    this.canvas.style.cursor = "crosshair";
+    this.resize()
+  }
+  this.resize = function () {
+    canvas.width = SCREEN_WIDTH;
+    canvas.height = SCREEN_HEIGHT;
+    canvas.style.position = 'absolute';
+    canvas.style.left = (window.innerWidth - SCREEN_WIDTH) * .5 + 'px';
+    canvas.style.top = (window.innerHeight - SCREEN_HEIGHT) * .5 + 'px';
+  }
+  this.clear = function () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+
