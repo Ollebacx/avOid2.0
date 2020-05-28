d942019a6f990b5e8e8296ede40e06d0aaffa540
Ollebacx <dav.ollebac@gmail.com>
2020-05-28 12:18:44 +0100

    little changes

--- js/map.js
+++ js/map.js
@@ -1,17 +1,17 @@
 
 function Map() {
   this.create = function () {
-    // this.canvas = config.canvas;
-    this.canvas = document.getElementById('canvas');
+    this.canvas = CANVAS;
     this.context = this.canvas.getContext('2d')
     this.canvas.style.cursor = "crosshair";
     this.resize()
   }
   this.resize = function () {
-    canvas.width = SCREEN_WIDTH;
-    canvas.height = SCREEN_HEIGHT;
-    canvas.style.position = 'absolute';
-    canvas.style.left = (window.innerWidth - SCREEN_WIDTH) * .5 + 'px';
+    // console.log(SCREEN_WIDTH)
+    this.canvas.width = SCREEN_WIDTH;
+    this.canvas.height = SCREEN_HEIGHT;
+    this.canvas.style.position = 'absolute';
+    this.canvas.style.left = (window.innerWidth - SCREEN_WIDTH) * .5 + 'px';
     canvas.style.top = (window.innerHeight - SCREEN_HEIGHT) * .5 + 'px';
   }
   this.clear = function () {
